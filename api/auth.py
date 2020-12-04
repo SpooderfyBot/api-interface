60562c3f647e27307755e5bdf923379bfad91dfd
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-12-04 22:32:19 +0000

    maybe?

--- api/auth.py
+++ api/auth.py
@@ -29,6 +29,7 @@ def make_redirect_url() -> str:
             f"&redirect_uri={urllib.parse.quote(REDIRECT_URL, safe='')}"
             "&response_type=code"
             "&scope=identify"
+            f"&state={create_session_id()}"
     )
 
 
@@ -111,15 +112,17 @@ class Authorization(router.Blueprint):
             'client_secret': CLIENT_SECRET,
             'grant_type': 'authorization_code',
             'code': code,
-            'redirect_uri': urllib.parse.quote(REDIRECT_URL, safe=''),
+            'redirect_uri': REDIRECT_URL,
             'scope': 'identify'
         }
-
-        query = f"?client_id={CLIENT_ID}"
-        query += "&".join([f"{k}={v}" for k, v in data.items()])
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
 
         async with self.session.post(
-                DISCORD_BASE_URL + DISCORD_OAUTH2_TOKEN + query,
+                DISCORD_BASE_URL + DISCORD_OAUTH2_TOKEN,
+                data=data,
+                headers=headers,
         ) as resp:
             if resp != 200:
                 return None
