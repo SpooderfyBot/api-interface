0b60f5f9cc4d52530248110e0c0019773e51781e
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-12-04 19:55:26 +0000

    redis and setup for api deploy

--- redis/connector.py
+++ redis/connector.py
@@ -20,7 +20,7 @@ class RedisManager:
         """
         for i, coll in enumerate(self._collections, start=1):
             print(f"[ REDIS ][ STATUS ] Creating pool {coll}")
-            self._pools[coll] = await aioredis.create_redis_pool(f"redis://127.0.0.1:6379/{i}")
+            self._pools[coll] = await aioredis.create_redis_pool(f"redis://redis:6379/{i}")
 
     async def shutdown(self):
         for name, pool in self._pools.items():
