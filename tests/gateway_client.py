49cacb94dc51bd98495ec69f4d3505356092129f
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-12-12 21:10:42 +0000

    can set this back as a int converter

--- tests/gateway_client.py
+++ tests/gateway_client.py
@@ -2,9 +2,36 @@ import aiohttp
 import asyncio
 
 
+async def read(ws: aiohttp.ClientWebSocketResponse, fut: asyncio.Future):
+    while not ws.closed:
+        print(await ws.receive())
+
+    fut.set_result(None)
+
+
+async def write(ws: aiohttp.ClientWebSocketResponse, fut: asyncio.Future):
+    pass
+
+
 async def main():
+    loop = asyncio.get_running_loop()
+
     session = aiohttp.ClientSession()
-    await session.ws_connect("ws://spooderfy.com/gateway?")
+    ws: aiohttp.ClientWebSocketResponse = await session.ws_connect(
+        "ws://spooderfy.com/gateway"
+        "?id=PFFFU"
+        "&session=c6c98bf6059f4037bc6de34eef87103b"
+    )
+    print("connected")
+
+    fut = loop.create_future()
+    loop.create_task(read(ws, fut))
+
+    await write(ws, fut)
+
+    await fut
+
+
 
 
 asyncio.run(main())
