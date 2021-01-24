0f7bbc1da4e334ba40554df31cd8b5e29edaddd6
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2021-01-24 20:37:38 +0000

    maybe fix this auth??

--- api/auth.py
+++ api/auth.py
@@ -111,7 +111,7 @@ class Authorization(router.Blueprint):
                 })
 
             redirect_to = request.cookies.pop("redirect_to", "/home")
-            resp = responses.RedirectResponse(redirect_to)
+            resp = responses.RedirectResponse(f"https://spooderfy.com{redirect_to}")
             resp.delete_cookie("redirect_to")
             resp.set_cookie("session", session_id, secure=True, samesite="strict")
 
